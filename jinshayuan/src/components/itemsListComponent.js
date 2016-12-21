c36ad6a9e49017048f367f28b7b69b66c49afbe3
fangzheng <chenfangzheng@outlook.com>
2016-12-21 15:15:49 +0800

    龙头企业-静态

---
+++ jinshayuan/src/components/itemsListComponent.js
@@ -0,0 +1,13 @@
+import './itemsListComponent.scss';
+
+export class ItemsList {
+    constructor(containerId, title) {
+        this.containerId = containerId;
+        this.title = title;
+    }
+
+    render(data) {
+        let html = `<h3 class="componentTitle">龙头企业 (工业产值)</h3>
+                        <ul>`;
+    }
+}
