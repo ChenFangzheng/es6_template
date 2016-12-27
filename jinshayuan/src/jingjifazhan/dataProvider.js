a48f366c99c5330b070f0abfc65d9a8943f5b48a
fangzheng <chenfangzheng@outlook.com>
2016-12-27 11:36:12 +0800

    data

--- jinshayuan/src/jingjifazhan/dataProvider.js
+++ jinshayuan/src/jingjifazhan/dataProvider.js
@@ -116,6 +116,10 @@ const data = {
             order: 1,
             name: "中机铸材科技(福建)有限公司",
             value: "2135万元"
+        }, {
+            order: 5,
+            name: "三明重型机器有限公司",
+            value: "322万元"
         }]
     }
 }
